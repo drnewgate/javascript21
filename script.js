a55457559e150c056cdcb43b67b2e5988a9befef
Aleksei Buhtin <alekseibuhtin@mail.ru>
2021-09-30 23:07:32 +0300

    new branch

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 alert ("Hello, World!"); // this is comment
+const title = 'Javascript';
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
 console.log(123);
- const title = 'Javascript';
- let screens;
- let screenPrice;
- let rollback;
- let fullPrice;
- let adaptive;
