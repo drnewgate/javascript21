2931854b41a5f16e42e6c4d723111d11ecf5d5bf
Aleksei Buhtin <alekseibuhtin@mail.ru>
2021-10-02 10:42:00 +0300

    add new brunch

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 alert ("Hello, World!"); // this is comment
 const title = 'Javascript';
+console.log(123);
 let screens;
 let screenPrice;
 let rollback;
 let fullPrice;
 let adaptive;
-console.log(123);
