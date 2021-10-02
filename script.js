64701f5f618417a95a300cf7bce1451d83e1bfa1
Aleksei Buhtin <alekseibuhtin@mail.ru>
2021-10-02 14:09:32 +0300

    add brunch lesson02 whith formuls

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-alert ("Hello, World!"); // this is comment
-const title = 'Javascript';
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
-console.log(123);
+
+const title = 'Типы данных, операторы, методы и свойства';
+console.log(typeof title);
+
+let screens = "Простые, Сложные, Интерактивные";
+console.log('Простые, Сложные, Интерактивные'.toLowerCase());
+console.log(screens.length);
+console.log(screens.split(","));
+
+let screenPrice= 11; 
+let fullPrice = 149752; 
+console.log('"Стоимость верстки экранов ' + screenPrice + ' рублей/ долларов/гривен/юани” и “Стоимость разработки сайта ' + fullPrice + ' рублей/ долларов/гривен/юани”');
+
+let rollback = 95; 
+console.log(fullPrice * (rollback/100));
+
+console.log(typeof fullPrice);
+
+let adaptive = true; 
+console.log(typeof adaptive);
