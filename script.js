49f54e34e865b8538edd35d3aa6972e70e8cd190
Aleksei Buhtin <alekseibuhtin@mail.ru>
2021-10-05 20:43:02 +0300

    add new formuls

--- script.js
+++ script.js
@@ -10,15 +10,18 @@ console.log('Простые, Сложные, Интерактивные'.toLower
 console.log(screens.length);
 console.log(screens.split(","));
 
-const screenPrice = prompt("Сколько будет стоить данная работа?")
+const screenPrice = prompt("Сколько будет стоить данная работа?");
 console.log (parseInt(screenPrice));
-if (true) console.log (screenPrice);
+if (true) {
+  console.log (screenPrice);
+}
 
-let fullPrice = servicePrice1 + servicePrice2;
+
+
 let rollback = 95; 
 console.log('"Стоимость верстки экранов ' + screenPrice + ' рублей/ долларов/гривен/юани” и “Стоимость разработки сайта ' + fullPrice + ' рублей/ долларов/гривен/юани”');
 let servicePercentPrice = Math.ceil(fullPrice * (rollback/100));
-console.log(fullPrice);
+console.log(servicePercentPrice);
 
 let adaptive = confirm('Нужен ли адаптив на сайте?');
 console.log(adaptive);
@@ -33,16 +36,15 @@ console.log(service2);
 let servicePrice2 = prompt("Сколько это будет стоить?");
 console.log(servicePrice2);
 
-if (fullPrice > 30000 ) {
-  console.log ('Даем скидку 10%');
+let fullPrice = Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
+console.log(fullPrice);
 
-switch(true) {
-  case 15000 <= fullPrice && fullPrice <= 30000;
-    console.log ('Даем скидку 5%'); 
-      break
-  case  0 <= fullPrice && fullPrice <= 15000;
-    console.log ('Скидка не предусмотрена');
-      break
-  default:
-    console.log ('Что-то пошло не так');
+if (fullPrice > 30000 ) {
+    console.log('Даем скидку в 10%');
+} else if (fullPrice > 15000 && fullPrice <30000) {
+    console.log('Даем скидку в 5%');
+} else if (fullPrice < 15000 && fullPrice > 0) {
+    console.log('Скидка не предусмотрена');
+} else {
+    console.log('Что-то пошло не так');
 }
